4b5eea25bc8200de99e4adcfb0ac6535365d0e6e
aabidraina6 <aabidraina5@gmail.com>
2024-02-15 21:32:30 +0530

    socketIo implemented-backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const socket = require("socket.io");    
+const socket = require("socket.io");
 
+const users = [];
+const messages = [];
 
-app.get("/", (req, res) => {  
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-const server  = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-const io = socket(server);  
+const io = socket(server);
+
+io.on("connection", (socket, name) => {
+  console.log("New client connected");
+  users.push({
+    id: socket.id,
+    name: name,
+  });
+  socket.emit("init", messages);
+  socket.on("message", (data) => {
+    messages.push(data);
+    io.emit("message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    users.splice(users.indexOf(socket.id), 1);
+  });
+});
