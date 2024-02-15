7139478c99f59cec980a2adce2bdc357207fe01a
aabidraina6 <aabidraina5@gmail.com>
2024-02-15 22:19:02 +0530

    backend-message

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,25 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const socket = require("socket.io");
+const cors = require("cors");
 
 const users = [];
 const messages = [];
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-const io = socket(server);
 
-io.on("connection", (socket, name) => {
+const io = socket(app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  }));
+
+io.on("connection", (socket) => {
   console.log("New client connected");
-  users.push({
-    id: socket.id,
-    name: name,
-  });
+
   socket.emit("init", messages);
   socket.on("message", (data) => {
     messages.push(data);
