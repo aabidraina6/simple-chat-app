dc63fba1432ea3c15d48af6555769e8b15a804e4
aabidraina6 <aabidraina5@gmail.com>
2024-02-15 21:23:35 +0530

    Socket.io instance intialized

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const socket = require("socket.io");    
+
 
 app.get("/", (req, res) => {  
     res.send("Hello World");
-    });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
+
+const server  = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+const io = socket(server);  
